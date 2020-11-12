cbb08aab86da2b90c1a61c90425eea18df74bffe
3A713107 <andd200088@gmail.com>
2020-11-12 18:34:09 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW01_3A713107/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WP_2020_01_HW01_3A713107
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGO = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGO
+            // 
+            this.btnGO.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGO.Location = new System.Drawing.Point(265, 248);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(215, 96);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "MD5加密";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbInput.Location = new System.Drawing.Point(212, 140);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(334, 42);
+            this.tbInput.TabIndex = 1;
+            this.tbInput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.btnGO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.RichTextBox tbInput;
+    }
+}
+
---
+++ WP_2020_01_HW01_3A713107/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace WP_2020_01_HW01_3A713107
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            MD5 md5 = MD5.Create();//建立一個MD5
+            byte[] source = Encoding.Default.GetBytes(tbInput.Text);//將字串轉為Byte[]
+            byte[] crypto = md5.ComputeHash(source);//進行MD5加密
+            string result = Convert.ToBase64String(crypto);//把加密後的字串從Byte[]轉為字串
+            MessageBox.Show("MD5加密:  " + result);//輸出結果
+        }
+    }
+}
